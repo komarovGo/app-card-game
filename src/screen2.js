91c250ce2f952e6f47d17c46b4ea6f3b795ebead
Georgy Komarov <komarovgosha001@gmail.com>
2023-02-02 18:15:49 +0300

    final

--- src/screen2.js
+++ src/screen2.js
@@ -1,10 +1,4 @@
-function screenGameCard() {
-    const conteiner = document.querySelector('.conteiner');
-    conteiner.innerHTML = '';
-
-    const wrapper = document.createElement('div');
-    wrapper.classList = 'wrapper wrapper__screen2';
-
+function BlocksGameCard() {
     const content = document.createElement('div');
     content.classList = 'content content__screen2';
 
@@ -25,11 +19,11 @@ function screenGameCard() {
     const watch = document.createElement('div');
     watch.classList = 'watch';
 
-    const m = document.createElement('div');
+    const m = document.createElement('h3');
     m.classList = 'minuts';
     m.textContent = '00.';
 
-    const s = document.createElement('div');
+    const s = document.createElement('h3');
     s.classList = 'seconds';
     s.textContent = '00';
 
@@ -40,8 +34,6 @@ function screenGameCard() {
     const cardField = document.createElement('div');
     cardField.classList = 'card-field';
 
-    conteiner.appendChild(wrapper);
-    wrapper.appendChild(content);
     content.appendChild(timer);
     timer.appendChild(minSec);
     minSec.appendChild(min);
@@ -51,13 +43,14 @@ function screenGameCard() {
     watch.appendChild(s);
     content.appendChild(timer);
     content.appendChild(buttonStartOver);
-    wrapper.appendChild(cardField);
 
-    window.application.renderBlock('renderoneBlockCardField', cardField);
+    window.application.conteiner.appendChild(content);
+    const nemberMin = document.getElementsByTagName('h3')[0];
+    const nemberSecond = document.getElementsByTagName('h3')[1];
 
     let peremenMinuts = 0;
     let peremenSeconds = 0;
-    function Tak() {
+    function tikTak() {
         peremenSeconds++;
         if (peremenSeconds <= 9) {
             s.innerHTML = `0${peremenSeconds}`;
@@ -70,245 +63,243 @@ function screenGameCard() {
             peremenSeconds = 0;
             m.innerHTML = `0${peremenMinuts}.`;
         }
+
+        window.application.watch = `${nemberMin.textContent}${nemberSecond.textContent}`;
     }
 
     function taimer() {
-        setInterval(Tak, 1000);
+        setInterval(tikTak, 1000);
     }
 
     setTimeout(() => {
         taimer();
-    }, 5000);
+    }, 1000);
 
     buttonStartOver.addEventListener('click', () => {
         window.application.renderScreen('renderone_screen');
     });
 }
 
-window.application.screens['renderoneScreenGameCard'] = screenGameCard;
+window.application.blocks['rendeBlocksGameCard'] = BlocksGameCard;
 
 function blockCardField() {
-    const cardFild = document.querySelector('.card-field');
-
     const tus = document.createElement('div');
+    tus.classList = 'box';
     const cardTB = document.createElement('img');
     cardTB.src = './static/card/b/t-b.svg';
     cardTB.classList = 'card';
     const cardTBshirt = document.createElement('img');
     cardTBshirt.src = './static/cover-card.svg';
-    cardTBshirt.classList = 'shirt shirt_pick';
+    cardTBshirt.classList = 'shirt';
 
     tus.appendChild(cardTB);
     tus.appendChild(cardTBshirt);
 
-    cardFild.appendChild(tus);
-
     const tus2 = document.createElement('div');
+    tus2.classList = 'box';
     const cardTCH = document.createElement('img');
     cardTCH.src = './static/card/ch/t-ch.svg';
     cardTCH.classList = 'card';
     const cardTCHshirt = document.createElement('img');
     cardTCHshirt.src = './static/cover-card.svg';
-    cardTCHshirt.classList = 'shirt shirt_pick';
+    cardTCHshirt.classList = 'shirt';
 
     tus2.appendChild(cardTCH);
     tus2.appendChild(cardTCHshirt);
 
-    cardFild.appendChild(tus2);
-
     const karol = document.createElement('div');
+    karol.classList = 'box';
     const cardKK = document.createElement('img');
     cardKK.src = './static/card/k/KK.svg';
     cardKK.classList = 'card';
     const cardKKshirt = document.createElement('img');
     cardKKshirt.src = './static/cover-card.svg';
-    cardKKshirt.classList = 'shirt shirt_pick';
+    cardKKshirt.classList = 'shirt';
 
     karol.appendChild(cardKK);
     karol.appendChild(cardKKshirt);
-    cardFild.appendChild(karol);
 
     const karol2 = document.createElement('div');
+    karol2.classList = 'box';
     const cardKP = document.createElement('img');
     cardKP.src = './static/card/P/KP.svg';
     cardKP.classList = 'card';
     const cardKPshirt = document.createElement('img');
     cardKPshirt.src = './static/cover-card.svg';
-    cardKPshirt.classList = 'shirt shirt_pick';
+    cardKPshirt.classList = 'shirt';
 
     karol2.appendChild(cardKP);
     karol2.appendChild(cardKPshirt);
-    cardFild.appendChild(karol2);
 
     const dama = document.createElement('div');
+    dama.classList = 'box';
     const cardDB = document.createElement('img');
     cardDB.src = './static/card/b/d-b.svg';
     cardDB.classList = 'card';
     const cardDBshirt = document.createElement('img');
     cardDBshirt.src = './static/cover-card.svg';
-    cardDBshirt.classList = 'shirt shirt_pick';
+    cardDBshirt.classList = 'shirt';
 
     dama.appendChild(cardDB);
     dama.appendChild(cardDBshirt);
-    cardFild.appendChild(dama);
 
     const dama2 = document.createElement('div');
+    dama2.classList = 'box';
     const cardDCH = document.createElement('img');
     cardDCH.src = './static/card/ch/d-ch.svg';
     cardDCH.classList = 'card';
     const cardDCHshirt = document.createElement('img');
     cardDCHshirt.src = './static/cover-card.svg';
-    cardDCHshirt.classList = 'shirt shirt_pick';
+    cardDCHshirt.classList = 'shirt';
 
     dama2.appendChild(cardDCH);
     dama2.appendChild(cardDCHshirt);
-    cardFild.appendChild(dama2);
 
     const valet = document.createElement('div');
+    valet.classList = 'box';
     const cardVK = document.createElement('img');
     cardVK.src = './static/card/k/VK.svg';
     cardVK.classList = 'card';
     const cardVKshirt = document.createElement('img');
     cardVKshirt.src = './static/cover-card.svg';
-    cardVKshirt.classList = 'shirt shirt_pick';
+    cardVKshirt.classList = 'shirt';
 
     valet.appendChild(cardVK);
     valet.appendChild(cardVKshirt);
-    cardFild.appendChild(valet);
 
     const valet2 = document.createElement('div');
+    valet2.classList = 'box';
     const cardVP = document.createElement('img');
     cardVP.src = './static/card/P/JP.svg';
     cardVP.classList = 'card';
     const cardVPshirt = document.createElement('img');
     cardVPshirt.src = './static/cover-card.svg';
-    cardVPshirt.classList = 'shirt shirt_pick';
+    cardVPshirt.classList = 'shirt';
 
     valet2.appendChild(cardVP);
     valet2.appendChild(cardVPshirt);
-    cardFild.appendChild(valet2);
 
     const ten = document.createElement('div');
+    ten.classList = 'box';
     const cardTenB = document.createElement('img');
     cardTenB.src = './static/card/b/10-b.svg';
     cardTenB.classList = 'card';
     const cardTenBshirt = document.createElement('img');
     cardTenBshirt.src = './static/cover-card.svg';
-    cardTenBshirt.classList = 'shirt shirt_pick';
+    cardTenBshirt.classList = 'shirt';
 
     ten.appendChild(cardTenB);
     ten.appendChild(cardTenBshirt);
-    cardFild.appendChild(ten);
 
     const ten2 = document.createElement('div');
+    ten2.classList = 'box';
     const cardTenCH = document.createElement('img');
     cardTenCH.src = './static/card/ch/10-ch.svg';
     cardTenCH.classList = 'card';
     const cardTenCHshirt = document.createElement('img');
     cardTenCHshirt.src = './static/cover-card.svg';
-    cardTenCHshirt.classList = 'shirt shirt_pick';
+    cardTenCHshirt.classList = 'shirt';
 
     ten2.appendChild(cardTenCH);
     ten2.appendChild(cardTenCHshirt);
-    cardFild.appendChild(ten2);
 
     const nine = document.createElement('div');
+    nine.classList = 'box';
     const cardNineK = document.createElement('img');
     cardNineK.src = './static/card/k/9K.svg';
     cardNineK.classList = 'card';
     const cardNineKshirt = document.createElement('img');
     cardNineKshirt.src = './static/cover-card.svg';
-    cardNineKshirt.classList = 'shirt shirt_pick';
+    cardNineKshirt.classList = 'shirt';
 
     nine.appendChild(cardNineK);
     nine.appendChild(cardNineKshirt);
-    cardFild.appendChild(nine);
 
     const nine2 = document.createElement('div');
+    nine2.classList = 'box';
     const cardNineP = document.createElement('img');
     cardNineP.src = './static/card/P/9P.svg';
     cardNineP.classList = 'card';
     const cardNinePshirt = document.createElement('img');
     cardNinePshirt.src = './static/cover-card.svg';
-    cardNinePshirt.classList = 'shirt shirt_pick';
+    cardNinePshirt.classList = 'shirt';
 
     nine2.appendChild(cardNineP);
     nine2.appendChild(cardNinePshirt);
-    cardFild.appendChild(nine2);
 
     const eight = document.createElement('div');
+    eight.classList = 'box';
     const cardEightB = document.createElement('img');
     cardEightB.src = './static/card/b/8-b.svg';
     cardEightB.classList = 'card';
     const cardEightBshirt = document.createElement('img');
     cardEightBshirt.src = './static/cover-card.svg';
-    cardEightBshirt.classList = 'shirt shirt_pick';
+    cardEightBshirt.classList = 'shirt';
 
     eight.appendChild(cardEightB);
     eight.appendChild(cardEightBshirt);
-    cardFild.appendChild(eight);
 
     const eight2 = document.createElement('div');
+    eight2.classList = 'box';
     const cardEightCH = document.createElement('img');
     cardEightCH.src = './static/card/ch/8-ch.svg';
     cardEightCH.classList = 'card';
     const cardEightCHshirt = document.createElement('img');
     cardEightCHshirt.src = './static/cover-card.svg';
-    cardEightCHshirt.classList = 'shirt shirt_pick';
+    cardEightCHshirt.classList = 'shirt';
 
     eight2.appendChild(cardEightCH);
     eight2.appendChild(cardEightCHshirt);
-    cardFild.appendChild(eight2);
 
     const seven = document.createElement('div');
+    seven.classList = 'box';
     const cardSevenK = document.createElement('img');
     cardSevenK.src = './static/card/k/7K.svg';
     cardSevenK.classList = 'card';
     const cardSevenKshirt = document.createElement('img');
     cardSevenKshirt.src = './static/cover-card.svg';
-    cardSevenKshirt.classList = 'shirt shirt_pick';
+    cardSevenKshirt.classList = 'shirt';
 
     seven.appendChild(cardSevenK);
     seven.appendChild(cardSevenKshirt);
-    cardFild.appendChild(seven);
 
     const seven2 = document.createElement('div');
+    seven2.classList = 'box';
     const cardSevenP = document.createElement('img');
     cardSevenP.src = './static/card/P/7P.svg';
     cardSevenP.classList = 'card';
     const cardSevenPshirt = document.createElement('img');
     cardSevenPshirt.src = './static/cover-card.svg';
-    cardSevenPshirt.classList = 'shirt shirt_pick';
+    cardSevenPshirt.classList = 'shirt';
 
     seven2.appendChild(cardSevenP);
     seven2.appendChild(cardSevenPshirt);
-    cardFild.appendChild(seven2);
 
     const six = document.createElement('div');
+    six.classList = 'box';
     const cardSixB = document.createElement('img');
     cardSixB.src = './static/card/b/6-b.svg';
     cardSixB.classList = 'card';
     const cardSixBshirt = document.createElement('img');
     cardSixBshirt.src = './static/cover-card.svg';
-    cardSixBshirt.classList = 'shirt shirt_pick';
+    cardSixBshirt.classList = 'shirt';
 
     six.appendChild(cardSixB);
     six.appendChild(cardSixBshirt);
-    cardFild.appendChild(six);
 
     const six2 = document.createElement('div');
+    six2.classList = 'box';
     const cardSixCH = document.createElement('img');
     cardSixCH.src = './static/card/ch/6-ch.svg';
     cardSixCH.classList = 'card';
     const cardSixCHshirt = document.createElement('img');
     cardSixCHshirt.src = './static/cover-card.svg';
-    cardSixCHshirt.classList = 'shirt shirt_pick';
+    cardSixCHshirt.classList = 'shirt';
 
     six2.appendChild(cardSixCH);
     six2.appendChild(cardSixCHshirt);
-    cardFild.appendChild(six2);
 
-    let faceCard = [
+    let boxCard = [
         tus,
         tus2,
         karol,
@@ -329,45 +320,92 @@ function blockCardField() {
         six2,
     ];
 
-    function sortCard(faceCard) {
-        return faceCard.sort(function () {
+    function sortCard(boxCard) {
+        return boxCard.sort(function () {
             return 0.5 - Math.random();
         });
     }
 
-    sortCard(faceCard);
+    sortCard(boxCard);
 
-    // window.application.cards = faceCard;
+    window.application.cards = boxCard;
 
-    // let shirtCard = [
-    //     cardTB,
-    //     cardTCH,
-    //     cardKK,
-    //     cardKP,
-    //     cardDB,
-    //     cardDCH,
-    //     cardVK,
-    //     cardVP,
-    //     cardTenB,
-    //     cardTenCH,
-    //     cardNineK,
-    //     cardNineP,
-    //     cardEightB,
-    //     cardEightCH,
-    //     cardSevenK,
-    //     cardSevenP,
-    //     cardSixB,
-    //     cardSixCH,
-    // ];
+    let shirtCard = [
+        cardTBshirt,
+        cardTCHshirt,
+        cardKKshirt,
+        cardKPshirt,
+        cardDBshirt,
+        cardDCHshirt,
+        cardVKshirt,
+        cardVPshirt,
+        cardTenBshirt,
+        cardTenCHshirt,
+        cardNineKshirt,
+        cardNinePshirt,
+        cardEightBshirt,
+        cardEightCHshirt,
+        cardSevenKshirt,
+        cardSevenPshirt,
+        cardSixBshirt,
+        cardSixCHshirt,
+    ];
 
-    // function fusShirtCard(shirtCard) {
-    //     for (let i = 0; i < shirtCard.length; i++) {
-    //         shirtCard[i].classList.remove('shirt_pick');
-    //     }
-    // }
-    // setTimeout((id) => {
-    //     fusShirtCard(shirtCard);
-    // }, 5000);
+    function reversCard(shirtCard) {
+        for (let i = 0; i < shirtCard.length; i++) {
+            shirtCard[i].classList.add('shirt_pick');
+        }
+    }
+    setTimeout(() => {
+        reversCard(shirtCard);
+    }, 5000);
+
+    boxCard.forEach((pick) => {
+        pick.addEventListener('click', (event) => {
+            const target = event.target;
+            target.classList.remove('shirt_pick');
+        });
+    });
 }
 
-window.application.blocks['renderoneBlockCardField'] = blockCardField;
+window.application.blocks['renderBlockCardField'] = blockCardField;
+
+function blockPlayingField() {
+    if (window.application.levels === '3') {
+        for (let i = 0; i < window.application.cards.length; i++) {
+            window.application.cardField.appendChild(
+                window.application.cards[i]
+            );
+            window.application.conteiner.appendChild(
+                window.application.cardField
+            );
+        }
+    } else if (window.application.levels === '2') {
+        for (let i = 6; i < window.application.cards.length; i++) {
+            window.application.cardField.appendChild(
+                window.application.cards[i]
+            );
+            window.application.conteiner.appendChild(
+                window.application.cardField
+            );
+        }
+    } else if (window.application.levels === '1') {
+        for (let i = 12; i < window.application.cards.length; i++) {
+            window.application.cardField.appendChild(
+                window.application.cards[i]
+            );
+            window.application.conteiner.appendChild(
+                window.application.cardField
+            );
+        }
+    }
+}
+
+window.application.blocks['renderBlockPlayingField'] = blockPlayingField;
+
+function renderGameScreen() {
+    window.application.renderBlock('rendeBlocksGameCard');
+    window.application.renderBlock('renderBlockCardField');
+    window.application.renderBlock('renderBlockPlayingField');
+}
+window.application.screens['renderHardGame'] = renderGameScreen;
