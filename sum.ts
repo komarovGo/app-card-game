8a19b4a3acd33c83d19af2f9f1d4d91c4d052ba1
Georgy Komarov <komarovgosha001@gmail.com>
2023-03-03 11:21:50 +0300

    asdf

--- sum.ts
+++ sum.ts
@@ -1,6 +1,6 @@
 function reversCard(shirtCard: any) {
     for (let i = 0; i < shirtCard.length; i++) {
-        shirtCard[i].classList.add('shirt_pick');
+        shirtCard[i].classList.add('shirt_pick');        
     }
 }
 module.exports = reversCard;
